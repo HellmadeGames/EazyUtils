12faf5fb89589fc38e94263ff93758fc53ff4000
Jack Hadjicosti <jack.hadjicosti@gmail.com>
2018-05-28 11:26:33 +0200

    Init commit
    - StringUtils
    - EditorUtils

---
+++ Assets/Hellmade/EazyUtils/Scripts/Editor/EditorUtils.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Hellmade.EazyUtils.Editor
+{
+    public class EditorUtils
+    {
+        /// <summary>
+        /// Focuses on an object in the editor
+        /// </summary>
+        /// <param name="obj"></param>
+        public static void FocusOnObject(GameObject obj)
+        {
+            Selection.activeGameObject = obj;
+            EditorGUIUtility.PingObject(obj);
+            SceneView.lastActiveSceneView.FrameSelected();
+        }
+
+        /// <summary>
+        /// Unfocuses from all controls in the editor
+        /// </summary>
+        public static void Unfocus()
+        {
+            GUIUtility.keyboardControl = 0;
+        }
+    }
+}
---
+++ Assets/Hellmade/EazyUtils/Scripts/StringUtils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hellmade.EazyUtils
+{
+    public class StringUtils
+    {
+        /// <summary>
+        /// Generates a unique string
+        /// </summary>
+        /// <returns>A unique alphanumeric string</returns>
+        public static string GenerateUniqueString()
+        {
+            long i = 1;
+            foreach (byte b in Guid.NewGuid().ToByteArray())
+            {
+                i *= ((int)b + 1);
+            }
+            return string.Format("{0:x}", i - DateTime.Now.Ticks);
+        }
+
+        /// <summary>
+        /// Checks if a string is unique among a list of other strings
+        /// </summary>
+        /// <param name="str">The string to be checked for uniqueness</param>
+        /// <param name="otherStrings">An array of strings to check the provided str against</param>
+        /// <returns>True if it is unique, false if it is not</returns>
+        public static bool IsUnique(string str, string[] otherStrings)
+        {
+            for (int i = 0; i < otherStrings.Length; i++)
+            {
+                if (str == otherStrings[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
