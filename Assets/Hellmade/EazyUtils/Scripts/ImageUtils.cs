ad9edf234b442b05d8210e8b9af8e1b6e9116576
Jack Hadjicosti <jack.hadjicosti@gmail.com>
2018-05-29 11:25:06 +0200

    Added generation of solid sprite
    - ImageUtils can now generate solid sprites too
    - Added function to convert texture2d to sprite

--- Assets/Hellmade/EazyUtils/Scripts/ImageUtils.cs
+++ Assets/Hellmade/EazyUtils/Scripts/ImageUtils.cs
@@ -7,13 +7,13 @@ namespace Hellmade.EazyUtils
     public class ImageUtils
     {
         /// <summary>
-        /// Geenrates a solid texture of a specific color and size
+        /// Genenrates a solid texture of a specific color and size
         /// </summary>
         /// <param name="width">The width of the generated texture</param>
         /// <param name="height">The height of the generated texture</param>
         /// <param name="color">The color of the generated texture</param>
         /// <returns>A solid texture2D</returns>
-        private static Texture2D GenerateSolidTexture(int width, int height, Color color)
+        public static Texture2D GenerateSolidTexture(int width, int height, Color color)
         {
             Color[] pix = new Color[width * height];
 
@@ -27,6 +27,28 @@ namespace Hellmade.EazyUtils
             tex.Apply();
 
             return tex;
+        }
+
+        /// <summary>
+        /// Genenrates a solid sprite of a specific color and size
+        /// </summary>
+        /// <param name="width">The width of the generated sprite</param>
+        /// <param name="height">The height of the generated sprite</param>
+        /// <param name="color">The color of the generated sprite</param>
+        /// <returns>A solid Sprite</returns>
+        public static Sprite GenerateSolidSprite(int width, int height, Color color)
+        {
+            return TextureToSprite(GenerateSolidTexture(width, height, color));
+        }
+
+        /// <summary>
+        /// Converts a Texture2D into a sprite
+        /// </summary>
+        /// <param name="texture">The texture to be converted</param>
+        /// <returns>A sprite</returns>
+        public static Sprite TextureToSprite(Texture2D texture)
+        {
+            return Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100.0f);
         }
     }
 }
