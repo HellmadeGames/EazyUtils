d1d7f0688b30ca4b5fbbe9ebb7e8af49f4904711
Jack Hadjicosti <jack.hadjicosti@gmail.com>
2018-06-05 09:08:52 +0200

    Added UIUtils
    - Helper for UI functionality

---
+++ Assets/Hellmade/EazyUtils/Scripts/UIUtils.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hellmade.EazyUtils.UI
+{
+    public class UIUtils : MonoBehaviour
+{
+    /// <summary>
+    /// Checks if a click/touch was done inside the boundaries of a UI object. Returns false if no click/touch was done at all
+    /// </summary>
+    /// <param name="UIObject"></param>
+    /// <returns></returns>
+    public static bool ClickedInsideUIObject(GameObject UIObject)
+    {
+        return ClickedInsideUIObject(UIObject.GetComponent<RectTransform>());
+    }
+
+    /// <summary>
+    /// Checks if a click/touch was done inside the boundaries of a UI object. Returns false if no click/touch was done at all
+    /// </summary>
+    /// <param name="UIObject"></param>
+    /// <returns></returns>
+    public static bool ClickedInsideUIObject(RectTransform UIObject)
+    {
+        return Input.GetMouseButton(0) &&
+            UIObject.gameObject.activeSelf && RectTransformUtility.RectangleContainsScreenPoint(UIObject, Input.mousePosition, Camera.main);
+    }
+}
