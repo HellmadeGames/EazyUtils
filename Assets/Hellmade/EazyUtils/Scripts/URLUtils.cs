820cc80fe64aa43ff92f9bd124b301fe7a061778
Jack Hadjicosti <jack.hadjicosti@gmail.com>
2018-06-05 09:20:42 +0200

    brackets fix

--- Assets/Hellmade/EazyUtils/Scripts/URLUtils.cs
+++ Assets/Hellmade/EazyUtils/Scripts/URLUtils.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 namespace Hellmade.EazyUtils.WWW
 {
     public class URLUtils : MonoBehaviour
-{
-
-    /// <summary>
-    /// Navigates to a given URL using the system's default internet broswer
-    /// </summary>
-    /// <param name="URL">The full URL to navigate to</param>
-    public static void GoToURL(string URL)
     {
-        Application.OpenURL(URL);
+
+        /// <summary>
+        /// Navigates to a given URL using the system's default internet broswer
+        /// </summary>
+        /// <param name="URL">The full URL to navigate to</param>
+        public static void GoToURL(string URL)
+        {
+            Application.OpenURL(URL);
+        }
     }
 }
