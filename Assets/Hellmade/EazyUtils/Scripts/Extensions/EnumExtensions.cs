82a1ae25b6553d8dc725cf759c9045991bdf339f
Jack Hadjicosti <jack.hadjicosti@gmail.com>
2018-07-11 08:50:51 +0200

    Added EnumExtensions

---
+++ Assets/Hellmade/EazyUtils/Scripts/Extensions/EnumExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hellmade.EazyUtils
+{
+	/// <summary>
+    /// Various extensions for the enums
+    /// </summary>
+	public static class EnumExtensions
+	{ 
+		/// <summary>
+        /// Returns the next value in an enum
+        /// </summary>
+        /// <returns></returns>
+		public static T GetNext<T>(this T src) where T : struct
+		{
+			if (!typeof(T).IsEnum) throw new ArgumentException(String.Format("Argumnent {0} is not an Enum", typeof(T).FullName));
+
+			T[] Arr = (T[])Enum.GetValues(src.GetType());
+			int j = Array.IndexOf<T>(Arr, src) + 1;
+			return (Arr.Length == j) ? Arr[0] : Arr[j];
+		}
+	}
+}
